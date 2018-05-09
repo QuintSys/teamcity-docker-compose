c493ee8bc79ea78d9d2c9d94d2b6a2f59853b33e
Erich Quintero <qbantek@gmail.com>
2018-05-09 14:53:45 -0400

    recommended settings for pg

--- postgres/set-pg-conf.sh
+++ postgres/set-pg-conf.sh
@@ -4,5 +4,16 @@
 # https://confluence.jetbrains.com/pages/viewpage.action?pageId=74847395#HowTo...-ConfigureNewlyInstalledPostgreSQLServer
 PG_CONF=/var/lib/postgresql/data/postgresql.conf
 
-grep -q -F 'synchronous_commit=off' $PG_CONF || echo 'synchronous_commit=off' >> $PG_CONF
+# The default value of shared_buffers parameter is too small and should be increased:
 grep -q -F 'shared_buffers=512MB' $PG_CONF || echo 'shared_buffers=512MB' >> $PG_CONF
+
+# For write-intensive applications such as TeamCity,
+# it is recommended to change some of the checkpoint-related parameters:
+#
+#For PostgreSQL 9.5 and later:
+grep -q -F 'max_wal_size = 1500MB' $PG_CONF || echo 'max_wal_size = 1500MB' >> $PG_CONF
+grep -q -F 'checkpoint_completion_target=0.9' $PG_CONF || echo 'checkpoint_completion_target=0.9' >> $PG_CONF
+
+# If TeamCity is the only application using the PostgreSQL database,
+# we recommend disabling the synchronous_commit parameter:
+grep -q -F 'synchronous_commit=off' $PG_CONF || echo 'synchronous_commit=off' >> $PG_CONF
